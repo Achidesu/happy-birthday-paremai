619ec667abcf355102325fdce04ab7a7484f9609
Achidesu <achizxcarchk@gmail.com>
2025-04-03 03:29:14 +0700

    1

---
+++ index.js
@@ -0,0 +1,3 @@
+const first_page_volume = document.getElementById("bg-sound");
+
+first_page_volume.volume = 0.1;
---
+++ small-gift.js
@@ -0,0 +1,3 @@
+const first_page_volume = document.getElementById("bg-video");
+
+first_page_volume.volume = 0.3;
